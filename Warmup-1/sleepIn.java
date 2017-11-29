6dc3e452b31a5194cd9ce01c583578fbce90bf0d
Veronika Pasechnik <paveronika@gmail.com>
2017-11-29 23:54:10 +0300

    first commit

---
+++ Warmup-1/sleepIn.java
@@ -0,0 +1,9 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  boolean sleepIn = true;
+  if (weekday != true || vacation != false){
+    return(true);
+  }
+  else{
+    return (false);
+  }
+}
